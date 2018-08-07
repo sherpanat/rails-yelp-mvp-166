0ff87cf5032f29eb4e3800cfe58e1302209576e9
Nathan Huberty <nathan.huberty@orange.fr>
2018-08-07 18:06:56 +0200

    validations all right

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ['chinese', 'japanese', 'french', 'belgian', 'italian']
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
